44574a52ba1c79b5c90fb694ac9ca480deb84fdd
Jenemina <122135250+Neminita@users.noreply.github.com>
2025-06-08 21:32:41 -0600

    Signup, y creación de vistas principales

--- digitalSolutions/urls.py
+++ digitalSolutions/urls.py
@@ -20,4 +20,6 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home, name='home'),
+    path('signup/', views.signup, name='signup')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
 # Create your views here.
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+        'form' :UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form' :UserCreationForm,
+                    'error':'Usuario ya existe'
+                })
+            
+        return render(request, 'signup.html', {
+                    'form' :UserCreationForm,
+                    'error':'Contraseñas no coinciden'
+                })
+
+
+def home(request):
+    return render(request, 'home.html',)
